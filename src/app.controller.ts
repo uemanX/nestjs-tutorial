458d6dd753992979d629bb07d5cb1332d3f38816
ueman <p20ym25q@gmail.com>
2021-07-05 22:54:14 +0900

    add: passport-jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -14,7 +18,6 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    console.log(req);
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne({ email });
     if (user && user.password === pass) {
@@ -11,5 +15,16 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    console.log(user);
+    console.log(user.email);
+    console.log(user.id);
+
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
